e7af3b6df0e9b635f9c1ba0a14e25358ab0aa70d
sila-ndeke <silahjeff138@gmail.com>
2025-03-01 11:00:33 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Hello, Flutter!")),
+        body: const Center(child: Text("Welcome to Flutter!")),
+      ),
+    );
+  }
+}
